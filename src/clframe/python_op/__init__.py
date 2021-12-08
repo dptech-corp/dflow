c1ececac5586a35b1cd27d9f42aa8f1ac93cc185
zjgemi <8871013+zjgemi@user.noreply.gitee.com>
2021-12-08 03:11:17 +0000

    !4 remove unused files * remove unused files * Merge branch 'master' into zjgemi * Merge branch 'master' into zjgemi * handle python environment * support for python op

--- src/clframe/python_op/__init__.py
+++
@@ -1,6 +0,0 @@
-from .op import OP
-from .opio import OPIOSign, OPIO
-from .op_parameter import OPParameterSign, OPParameter
-from .op_artifact import OPArtifactSign, OPArtifact
-from .python_op_template import PythonOPTemplate
-from .utils import handle_output
