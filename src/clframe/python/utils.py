c8774ee40b9530467ea4c6bd1ed3b15c4d16f6b6
zjgemi <8871013+zjgemi@user.noreply.gitee.com>
2021-12-13 05:24:26 +0000

    !11 retain parents of artifacts in python op * retain parents of artifacts in python op * Merge branch 'master' into zjgemi * fix a bug for float * Merge branch 'master' into zjgemi * support for progress reporting in python OP * Merge branch 'master' into zjgemi * name the new template * Merge branch 'master' into zjgemi * support for passing artifacts by pvc * Merge branch 'master' into zjgemi * Merge branch 'master' into zjgemi * refact the code * Merge branch 'master' into zjgemi * remove unused files * Merge branch 'master' into zjgemi * Merge branch 'master' into zjgemi * handle python environment * support for python op

--- src/clframe/python/utils.py
+++ src/clframe/python/utils.py
@@ -6,17 +6,9 @@ def handle_output(output_parameter, output_artifact):
     for name, value in output_parameter.items():
         open('/tmp/outputs/parameters/' + name, 'w').write(str(value))
     for name, value in output_artifact.items():
+        os.makedirs('/tmp/outputs/artifacts/' + name, exist_ok=True)
         if isinstance(value, set):
-            os.makedirs('/tmp/outputs/artifacts/' + name, exist_ok=True)
-            for i, path in enumerate(value):
-                create_hard_link(path, '/tmp/outputs/artifacts/%s/%s' % (name, i))
+            for path in value:
+                os.system("cp -lr --parents %s /tmp/outputs/artifacts/%s" % (path, name))
         else:
-            create_hard_link(value, '/tmp/outputs/artifacts/' + name)
-
-def create_hard_link(src, dst):
-    if os.path.isdir(src):
-        shutil.copytree(src, dst, copy_function=os.link)
-    elif os.path.isfile(src):
-        os.link(src, dst)
-    else:
-        raise RuntimeError("File %s not found" % src)
+            os.system("cp -lr --parents %s /tmp/outputs/artifacts/%s" % (value, name))
