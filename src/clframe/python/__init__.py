e19e74ba6a12a26efe13d67727cdb88d304cb406
zjgemi <x.liu@XdeMacBook-Pro.local>
2021-12-16 10:40:02 +0800

    redefine python op interface

--- src/clframe/python/__init__.py
+++ src/clframe/python/__init__.py
@@ -1,6 +1,4 @@
 from .op import OP
-from .opio import OPIOSign, OPIO
-from .op_parameter import OPParameterSign, OPParameter
-from .op_artifact import OPArtifactSign, OPArtifact
+from .opio import OPIOSign, OPIO, ArtifactPath
 from .python_op_template import PythonOPTemplate
 from .utils import handle_output
