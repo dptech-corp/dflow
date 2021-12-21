daf53bd9ef21bc90d9ef51241f615441743408c1
zjgemi <x.liu@XdeMacBook-Pro.local>
2021-12-21 11:30:51 +0800

    support for uploading directory

--- src/dflow/utils.py
+++ src/dflow/utils.py
@@ -1,16 +1,34 @@
 import os
 import uuid
+import tarfile
 from minio import Minio
 from argo.workflows.client.configuration import Configuration
 from argo.workflows.client import V1alpha1S3Artifact
 
-def download_artifact(artifact, **kwargs):
+def download_artifact(artifact, extract=True, **kwargs):
     if hasattr(artifact, "s3"):
-        return download_s3(key=artifact.s3.key, **kwargs)
+        path = download_s3(key=artifact.s3.key, **kwargs)
+        if path[-4:] == ".tgz" and extract:
+            tf = tarfile.open(path, "r:gz")
+            dirname = os.path.dirname(path)
+            tf.extractall(dirname)
+            path = list(map(lambda x: os.path.join(dirname, x), tf.getnames()))
+            tf.close()
+        return path
     else:
         raise NotImplementedError()
 
 def upload_artifact(path, **kwargs):
+    if os.path.isfile(path):
+        pass
+    elif os.path.isdir(path):
+        tf = tarfile.open(path + ".tgz", "w:gz")
+        tf.add(path, arcname=os.path.basename(path))
+        tf.close()
+        path = path + ".tgz"
+    else:
+        raise RuntimeError("File or directory %s not found" % path)
+
     key = upload_s3(path=path, **kwargs)
     config = Configuration()
     config.client_side_validation = False
@@ -19,7 +37,8 @@ def upload_artifact(path, **kwargs):
 def download_s3(key, path=None, endpoint="127.0.0.1:9000", access_key="admin", secret_key="password",
             secure=False, bucket_name="my-bucket", **kwargs):
     if path is None:
-        path = os.path.basename(key)
+        path = "."
+    path = os.path.join(path, os.path.basename(key))
     client = Minio(endpoint=endpoint, access_key=access_key, secret_key=secret_key, secure=secure)
     client.fget_object(bucket_name=bucket_name, object_name=key, file_path=path)
     return path
@@ -27,7 +46,7 @@ def download_s3(key, path=None, endpoint="127.0.0.1:9000", access_key="admin", s
 def upload_s3(path, key=None, endpoint="127.0.0.1:9000", access_key="admin", secret_key="password",
             secure=False, bucket_name="my-bucket", **kwargs):
     if key is None:
-        key = "upload/%s" % uuid.uuid4()
+        key = "upload/%s/%s" % (uuid.uuid4(), os.path.basename(path))
     client = Minio(endpoint=endpoint, access_key=access_key, secret_key=secret_key, secure=secure)
     client.fput_object(bucket_name=bucket_name, object_name=key, file_path=path)
     return key
