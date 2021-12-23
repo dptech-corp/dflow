de6f0bb00875821d74c71f5d7783153cc7806a10
zjgemi <x.liu@XdeMacBook-Pro.local>
2021-12-23 16:33:55 +0800

    upload artifact without archive

--- src/dflow/utils.py
+++ src/dflow/utils.py
@@ -8,7 +8,7 @@ from .io import S3Artifact
 def download_artifact(artifact, extract=True, **kwargs):
     if hasattr(artifact, "s3"):
         if hasattr(artifact, "archive") and hasattr(artifact.archive, "none") and artifact.archive.none is not None:
-            path = download_s3(key=artifact.s3.key, recursive=True, flat_first_dir=True, **kwargs)
+            path = download_s3(key=artifact.s3.key, recursive=True, **kwargs)
             return path
 
         path = download_s3(key=artifact.s3.key, recursive=False, **kwargs)
@@ -33,40 +33,31 @@ def download_artifact(artifact, extract=True, **kwargs):
     else:
         raise NotImplementedError()
 
-def upload_artifact(path, **kwargs):
+def upload_artifact(path, archive="tar", **kwargs):
     assert os.path.exists(path), "File or directory %s not found" % path
-    isdir = False
-    if os.path.isdir(path):
+    arch = False
+    if os.path.isdir(path) and archive == "tar":
         tf = tarfile.open(path + ".tgz", "w:gz")
         tf.add(path, arcname=os.path.basename(path))
         tf.close()
         path = path + ".tgz"
-        isdir = True
+        arch = True
 
     key = upload_s3(path=path, **kwargs)
-    if isdir: os.remove(path)
+    if arch: os.remove(path)
     return S3Artifact(key=key)
 
-def download_s3(key, path=None, recursive=True, flat_first_dir=True, endpoint="127.0.0.1:9000",
+def download_s3(key, path=None, recursive=True, endpoint="127.0.0.1:9000",
             access_key="admin", secret_key="password", secure=False, bucket_name="my-bucket", **kwargs):
     if path is None:
         path = "."
     client = Minio(endpoint=endpoint, access_key=access_key, secret_key=secret_key, secure=secure)
     if recursive:
-        name_list = [obj.object_name for obj in client.list_objects(bucket_name=bucket_name, prefix=key, recursive=True)]
-        if flat_first_dir:
-            rel_path = name_list[0][len(key):]
-            i = rel_path.find("/", 1)
-            if i != -1:
-                first_dir = key + rel_path[:i+1]
-                if all(map(lambda x: x[:len(first_dir)] == first_dir, name_list)):
-                    key = first_dir
-
-        for obj_name in name_list:
-            rel_path = obj_name[len(key):]
+        for obj in client.list_objects(bucket_name=bucket_name, prefix=key, recursive=True):
+            rel_path = obj.obj_name[len(key):]
             if rel_path[0] == "/": rel_path = rel_path[1:]
             file_path = os.path.join(path, rel_path)
-            client.fget_object(bucket_name=bucket_name, object_name=obj_name, file_path=file_path)
+            client.fget_object(bucket_name=bucket_name, object_name=obj.obj_name, file_path=file_path)
     else:
         path = os.path.join(path, os.path.basename(key))
         client.fget_object(bucket_name=bucket_name, object_name=key, file_path=path)
@@ -77,7 +68,17 @@ def upload_s3(path, key=None, endpoint="127.0.0.1:9000", access_key="admin", sec
     if key is None:
         key = "upload/%s/%s" % (uuid.uuid4(), os.path.basename(path))
     client = Minio(endpoint=endpoint, access_key=access_key, secret_key=secret_key, secure=secure)
-    client.fput_object(bucket_name=bucket_name, object_name=key, file_path=path)
+    if os.path.isfile(path):
+        client.fput_object(bucket_name=bucket_name, object_name=key, file_path=path)
+    elif os.path.isdir(path):
+        for dn, ds, fs in os.walk(path):
+            rel_path = dn[len(path):]
+            if rel_path == "":
+                pass
+            elif rel_path[0] != "/":
+                rel_path = "/" + rel_path
+            for f in fs:
+                client.fput_object(bucket_name=bucket_name, object_name="%s%s/%s" % (key, rel_path, f), file_path=os.path.join(dn, f))
     return key
 
 def merge_dir(src, dst):
