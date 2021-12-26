fa4597bc26b940ae5f2f91b197e874872f08d532
zjgemi <8871013+zjgemi@user.noreply.gitee.com>
2021-12-26 03:16:36 +0000

    !23 handle path of single artifact for python OP * handle path of single artifact for python OP * Merge branch 'master' into zjgemi * output parameter from parameter; output artifact from artifact; output… * Merge branch 'master' into zjgemi * remove required arguments from io * Merge branch 'master' into zjgemi * support for artifact list in python OP * Merge branch 'master' into zjgemi * upload artifact without archive * add archive, sub_path, save to artifact, with_param to step * Merge branch 'master' into zjgemi * remove root directory when download artifact * Merge branch 'master' into zjgemi * support for uploading directory * Merge branch 'master' into zjgemi * add tools for upload/download artifacts; rename clframe to dflow * Merge branch 'master' into zjgemi * redefine python op interface * Merge branch 'master' into zjgemi * fix a bug in debug mode * Merge branch 'master' into zjgemi * debug mode * Merge branch 'master' into zjgemi * retain parents of artifacts in python op * Merge branch 'master' into zjgemi * fix a bug for float * Merge branch 'master' into zjgemi * support for progress reporting in python OP * Merge branch 'master' into zjgemi * name the new template * Merge branch 'master' into zjgemi * support for passing artifacts by pvc * Merge branch 'master' into zjgemi * Merge branch 'master' into zjgemi * refact the code * Merge branch 'master' into zjgemi * remove unused files * Merge branch 'master' into zjgemi * Merge branch 'master' into zjgemi * handle python environment * support for python op

--- src/dflow/python/utils.py
+++ src/dflow/python/utils.py
@@ -8,24 +8,29 @@ def handle_input_artifacts(input, sign):
     for name, sign in sign.items():
         if isinstance(sign, Artifact):
             art_path = '/tmp/inputs/artifacts/%s' % name
+            path_list = [art_path]
+            if os.path.exists('%s/.dflow' % art_path):
+                with open('%s/.dflow' % art_path, 'r') as f:
+                    path_list = jsonpickle.loads(f.read())['path_list']
+                    path_list = list(map(lambda x: os.path.join(art_path, x), path_list))
             if sign.type == str:
-                input[name] = art_path
+                if len(path_list) == 1:
+                    input[name] = path_list[0]
+                else:
+                    input[name] = art_path
             elif sign.type == Path:
-                input[name] = Path(art_path)
-            elif sign.type in [Set[str], Set[Path], List[str], List[Path]]:
-                path_list = [art_path]
-                if os.path.exists('%s/.dflow' % art_path):
-                    with open('%s/.dflow' % art_path, 'r') as f:
-                        path_list = jsonpickle.loads(f.read())['path_list']
-                        path_list = list(map(lambda x: os.path.join(art_path, x), path_list))
-                if sign.type == List[str]:
-                    input[name] = path_list
-                elif sign.type == Set[str]:
-                    input[name] = set(path_list)
-                elif sign.type == List[Path]:
-                    input[name] = list(map(Path, path_list))
-                elif sign.type == Set[Path]:
-                    input[name] = set(map(Path, path_list))
+                if len(path_list) == 1:
+                    input[name] = Path(path_list[0])
+                else:
+                    input[name] = Path(art_path)
+            elif sign.type == List[str]:
+                input[name] = path_list
+            elif sign.type == Set[str]:
+                input[name] = set(path_list)
+            elif sign.type == List[Path]:
+                input[name] = list(map(Path, path_list))
+            elif sign.type == Set[Path]:
+                input[name] = set(map(Path, path_list))
 
 def handle_output(output, sign):
     os.makedirs('/tmp/outputs/parameters', exist_ok=True)
@@ -36,13 +41,11 @@ def handle_output(output, sign):
             path_list = []
             if sign.type in [str, Path]:
                 os.makedirs('/tmp/outputs/artifacts/' + name, exist_ok=True)
-                copy_results(value, name)
-                path_list.append(str(value))
+                path_list.append(copy_results(value, name))
             elif sign.type in [List[str], List[Path], Set[str], Set[Path]]:
                 os.makedirs('/tmp/outputs/artifacts/' + name, exist_ok=True)
                 for path in value:
-                    copy_results(path, name)
-                    path_list.append(str(path))
+                    path_list.append(copy_results(path, name))
             with open("/tmp/outputs/artifacts/%s/.dflow" % name, "w") as f:
                 f.write(jsonpickle.dumps({"path_list": path_list}))
         elif sign == str:
@@ -56,9 +59,11 @@ def copy_results(source, name):
         rel_path = source[source.find("/", len("/tmp/inputs/artifacts/"))+1:] # retain original directory structure
         target = "/tmp/outputs/artifacts/%s/%s" % (name, rel_path)
         copy_file(source, target, shutil.copy)
+        return rel_path
     else:
         target = "/tmp/outputs/artifacts/%s/%s" % (name, source)
         copy_file(source, target, os.link)
+        return source
 
 def copy_file(src, dst, func=os.link):
     os.makedirs(os.path.abspath(os.path.dirname(dst)), exist_ok=True)
