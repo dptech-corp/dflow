174c894b0991962f8d552fc351f2b28921a076ae
zjgemi <x.liu@XdeMacBook-Pro.local>
2022-01-04 11:31:20 +0800

    slice input parameter for python OP

--- src/dflow/python/python_op_template.py
+++ src/dflow/python/python_op_template.py
@@ -5,7 +5,7 @@ from ..io import Inputs, Outputs, InputParameter, OutputParameter, InputArtifact
 
 class PythonOPTemplate(PythonScriptOPTemplate):
     def __init__(self, op_class, image=None, command=None, input_artifact_slices=None, output_artifact_save=None,
-                 output_artifact_archive=None):
+                 output_artifact_archive=None, input_parameter_slices=None):
         class_name = op_class.__name__
         input_sign = op_class.get_input_sign()
         output_sign = op_class.get_output_sign()
@@ -18,20 +18,19 @@ class PythonOPTemplate(PythonScriptOPTemplate):
         if output_artifact_archive is not None:
             for name, archive in output_artifact_archive.items():
                 output_sign[name].archive = archive
-        inputs = Inputs()
-        outputs = Outputs()
-        dflow_vars = {}
+        super().__init__(name=class_name, inputs=Inputs(), outputs=Outputs())
+        self.dflow_vars = {}
         for name, sign in input_sign.items():
             if isinstance(sign, Artifact):
-                inputs.artifacts[name] = InputArtifact(path="/tmp/inputs/artifacts/" + name, optional=sign.optional)
+                self.inputs.artifacts[name] = InputArtifact(path="/tmp/inputs/artifacts/" + name, optional=sign.optional)
             else:
-                inputs.parameters[name] = InputParameter()
+                self.inputs.parameters[name] = InputParameter()
         for name, sign in output_sign.items():
             if isinstance(sign, Artifact):
-                outputs.artifacts[name] = OutputArtifact(path="/tmp/outputs/artifacts/" + name,
+                self.outputs.artifacts[name] = OutputArtifact(path="/tmp/outputs/artifacts/" + name,
                     archive=sign.archive, save=sign.save)
             else:
-                outputs.parameters[name] = OutputParameter(value_from_path="/tmp/outputs/parameters/" + name)
+                self.outputs.parameters[name] = OutputParameter(value_from_path="/tmp/outputs/parameters/" + name)
 
         script = ""
         if op_class.__module__ == "__main__":
@@ -41,35 +40,24 @@ class PythonOPTemplate(PythonScriptOPTemplate):
 
         script += "import jsonpickle\n"
         script += "from dflow.python import OPIO\n"
-        script += "from dflow.python.utils import handle_input_artifact, handle_output\n"
+        script += "from dflow.python.utils import handle_input_artifact, handle_input_parameter, handle_output\n"
         script += "from %s import %s\n\n" % (op_class.__module__, class_name)
         script += "op_obj = %s()\n" % class_name
         script += "input = OPIO()\n"
         script += "input_sign = %s.get_input_sign()\n" % class_name
         for name, sign in input_sign.items():
             if isinstance(sign, Artifact):
-                if sign.slices is not None:
-                    i = sign.slices.find("{{item")
-                    while i >= 0:
-                        j = sign.slices.find("}}", i+2)
-                        var = sign.slices[i:j+2]
-                        if var not in dflow_vars:
-                            var_name = "dflow_var_%s" % len(dflow_vars)
-                            inputs.parameters[var_name] = InputParameter(value=var)
-                            dflow_vars[var] = var_name
-                        else:
-                            var_name = dflow_vars[var]
-                        sign.slices = sign.slices.replace(var, "{{inputs.parameters.%s}}" % var_name)
-                        i = sign.slices.find("{{item")
+                sign.slices = self.render_slices(sign.slices)
                 script += "input['%s'] = handle_input_artifact('%s', input_sign['%s'], %s)\n" % (name, name, name, sign.slices)
-            elif sign == str:
-                script += "input['%s'] = '{{inputs.parameters.%s}}'\n" % (name, name)
             else:
-                script += "input['%s'] = jsonpickle.loads('{{inputs.parameters.%s}}')\n" % (name, name)
+                slices = None
+                if input_parameter_slices is not None:
+                    slices = self.render_slices(input_parameter_slices.get(name, None))
+                script += "input['%s'] = handle_input_parameter('%s', '{{inputs.parameters.%s}}', input_sign['%s'], %s)\n" % (name, name, name, name, slices)
+
         script += "output = op_obj.execute(input)\n"
         script += "handle_output(output, %s.get_output_sign())\n" % class_name
 
-        super().__init__(name=class_name, inputs=inputs, outputs=outputs)
         self.image = image
         if command is not None:
             self.command = command
@@ -77,3 +65,21 @@ class PythonOPTemplate(PythonScriptOPTemplate):
             self.command = ["python"]
         self.script = script
         self.init_progress = "%s/%s" % (op_class.progress_current, op_class.progress_total)
+    
+    def render_slices(self, slices=None):
+        if slices is None:
+            return None
+
+        i = slices.find("{{item")
+        while i >= 0:
+            j = slices.find("}}", i+2)
+            var = slices[i:j+2]
+            if var not in self.dflow_vars:
+                var_name = "dflow_var_%s" % len(self.dflow_vars)
+                self.inputs.parameters[var_name] = InputParameter(value=var)
+                self.dflow_vars[var] = var_name
+            else:
+                var_name = self.dflow_vars[var]
+            slices = slices.replace(var, "{{inputs.parameters.%s}}" % var_name)
+            i = slices.find("{{item")
+        return slices
