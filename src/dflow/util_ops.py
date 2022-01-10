5add0ffd5a248f4a8a975229fc4cb8e6fe81a1e6
zjgemi <8871013+zjgemi@user.noreply.gitee.com>
2022-01-10 01:31:52 +0000

    !46 add continue_on_success_ratio * add continue_on_success_ratio * Merge branch 'master' into zjgemi * add continue_on_num_success * Merge branch 'master' into zjgemi * add timeout and retry strategy * Merge branch 'master' into zjgemi * upload python packages to python path * Merge branch 'master' into zjgemi * add global name for output artifact * Merge branch 'master' into zjgemi * fix a bug * Merge branch 'master' into zjgemi * support for upload a list of path * Merge branch 'master' into zjgemi * stack slices to a S3Artifact and not archive for default * Merge branch 'master' into zjgemi * support for stacking output parameters and artifacts; add a compact in… * Merge branch 'master' into zjgemi * Merge branch 'master' into zjgemi * update examples * Merge branch 'master' into zjgemi * fix a bug in handling input artifact * Merge branch 'master' into zjgemi * slice input parameter for python OP * Merge branch 'master' into zjgemi * regex for query step * Merge branch 'master' into zjgemi * add arguments to PythonOPTemplate as patches to python OP; add argo_range * Merge branch 'master' into zjgemi * use slices of python artifact * Merge branch 'master' into zjgemi * optional artifact * Merge branch 'master' into zjgemi * handle path of single artifact for python OP * Merge branch 'master' into zjgemi * output parameter from parameter; output artifact from artifact; output… * Merge branch 'master' into zjgemi * remove required arguments from io * Merge branch 'master' into zjgemi * support for artifact list in python OP * Merge branch 'master' into zjgemi * upload artifact without archive * add archive, sub_path, save to artifact, with_param to step * Merge branch 'master' into zjgemi * remove root directory when download artifact * Merge branch 'master' into zjgemi * support for uploading directory * Merge branch 'master' into zjgemi * add tools for upload/download artifacts; rename clframe to dflow * Merge branch 'master' into zjgemi * redefine python op interface * Merge branch 'master' into zjgemi * fix a bug in debug mode * Merge branch 'master' into zjgemi * debug mode * Merge branch 'master' into zjgemi * retain parents of artifacts in python op * Merge branch 'master' into zjgemi * fix a bug for float * Merge branch 'master' into zjgemi * support for progress reporting in python OP * Merge branch 'master' into zjgemi * name the new template * Merge branch 'master' into zjgemi * support for passing artifacts by pvc * Merge branch 'master' into zjgemi * Merge branch 'master' into zjgemi * refact the code * Merge branch 'master' into zjgemi * remove unused files * Merge branch 'master' into zjgemi * Merge branch 'master' into zjgemi * handle python environment * support for python op

---
+++ src/dflow/util_ops.py
@@ -0,0 +1,19 @@
+from .op_template import ShellOPTemplate
+from .io import Inputs, InputParameter
+
+class CheckNumSuccess(ShellOPTemplate):
+    def __init__(self, name="check-num-success", image=None):
+        super().__init__(name=name, image=image)
+        self.command=["sh"]
+        self.script = "succ=`echo {{inputs.parameters.success}} | grep -o 1 | wc -l`\n"
+        self.script += "exit $(( $succ < {{inputs.parameters.threshold}} ))"
+        self.inputs = Inputs(parameters={"success": InputParameter(), "threshold": InputParameter()})
+
+class CheckSuccessRatio(ShellOPTemplate):
+    def __init__(self, name="check-success-ratio", image=None):
+        super().__init__(name=name, image=image)
+        self.command=["sh"]
+        self.script = "succ=`echo {{inputs.parameters.success}} | grep -o 1 | wc -l`\n"
+        self.script += "fail=`echo {{inputs.parameters.success}} | grep -o 0 | wc -l`\n"
+        self.script += "exit `echo $succ $fail | awk -v r={{inputs.parameters.threshold}} '{if ($1 < ($1+$2)*r) {print 1} else {print 0}}'`"
+        self.inputs = Inputs(parameters={"success": InputParameter(), "threshold": InputParameter()})
