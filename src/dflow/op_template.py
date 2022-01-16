81622de5966ad32c92aef601cb8d80642c573388
zjgemi <x.liu@XdeMacBook-Pro.local>
2022-01-16 21:26:45 +0800

    add memoization

--- src/dflow/op_template.py
+++ src/dflow/op_template.py
@@ -2,9 +2,13 @@ from argo.workflows.client import (
     V1alpha1Template,
     V1alpha1ScriptTemplate,
     V1VolumeMount,
-    V1alpha1Metadata
+    V1alpha1Metadata,
+    V1ConfigMapKeySelector,
+    V1alpha1Memoize,
+    V1alpha1Cache
 )
-from .io import Inputs, Outputs
+from argo.workflows.client.configuration import Configuration
+from .io import Inputs, Outputs, InputParameter
 
 class OPTemplate:
     def __init__(self, name, inputs=None, outputs=None):
@@ -18,52 +22,55 @@ class OPTemplate:
         else:
             self.outputs = Outputs()
 
-class ShellOPTemplate(OPTemplate):
+class ScriptOPTemplate(OPTemplate):
     def __init__(self, name, inputs=None, outputs=None, image=None, command=None, script=None, mounts=None,
-            init_progress="0/1", timeout=None, retry_strategy=None):
+            init_progress="0/1", timeout=None, retry_strategy=None, memoize_key=None):
         super().__init__(name=name, inputs=inputs, outputs=outputs)
         self.image = image
+        self.command = command
+        self.script = script
+        if mounts is None:
+            mounts = []
+        self.mounts = mounts
+        self.init_progress = init_progress
+        self.timeout = timeout
+        self.retry_strategy = retry_strategy
+        self.memoize_key = memoize_key
+
+    def get_memoize(self):
+        if self.memoize_key is not None:
+            # Is it a bug of Argo?
+            if self.memoize_key.find("workflow.name") != -1:
+                self.inputs.parameters["workflow_name"] = InputParameter(value="{{workflow.name}}")
+                self.memoize_key = self.memoize_key.replace("workflow.name", "inputs.parameters.workflow_name")
+            config = Configuration()
+            config.client_side_validation = False
+            return V1alpha1Memoize(key=self.memoize_key, local_vars_configuration=config, cache=V1alpha1Cache(config_map=V1ConfigMapKeySelector(name="dflow-config", local_vars_configuration=config)))
+        return None
+
+    def convert_to_argo(self):
+        memoize = self.get_memoize()
+        return V1alpha1Template(name=self.name,
+            metadata=V1alpha1Metadata(annotations={"workflows.argoproj.io/progress": self.init_progress}),
+            inputs=self.inputs.convert_to_argo(),
+            outputs=self.outputs.convert_to_argo(),
+            timeout=self.timeout,
+            retry_strategy=self.retry_strategy,
+            memoize=memoize,
+            script=V1alpha1ScriptTemplate(image=self.image, command=self.command, source=self.script, volume_mounts=self.mounts))
+
+class ShellOPTemplate(ScriptOPTemplate):
+    def __init__(self, name, inputs=None, outputs=None, image=None, command=None, script=None, mounts=None,
+            init_progress="0/1", timeout=None, retry_strategy=None, memoize_key=None):
         if command is None:
             command = ["sh"]
-        self.command = command
-        self.script = script
-        if mounts is None:
-            mounts = []
-        self.mounts = mounts
-        self.init_progress = init_progress
-        self.timeout = timeout
-        self.retry_strategy = retry_strategy
+        super().__init__(name=name, inputs=inputs, outputs=outputs, image=image, command=command, script=script,
+                mounts=mounts, init_progress=init_progress, timeout=timeout, retry_strategy=retry_strategy, memoize_key=memoize_key)
 
-    def convert_to_argo(self):
-        return V1alpha1Template(name=self.name,
-            metadata=V1alpha1Metadata(annotations={"workflows.argoproj.io/progress": self.init_progress}),
-            inputs=self.inputs.convert_to_argo(),
-            outputs=self.outputs.convert_to_argo(),
-            timeout=self.timeout,
-            retry_strategy=self.retry_strategy,
-            script=V1alpha1ScriptTemplate(image=self.image, command=self.command, source=self.script, volume_mounts=self.mounts))
-
-class PythonScriptOPTemplate(OPTemplate):
+class PythonScriptOPTemplate(ScriptOPTemplate):
     def __init__(self, name, inputs=None, outputs=None, image=None, command=None, script=None, mounts=None,
-            init_progress="0/1", timeout=None, retry_strategy=None):
-        super().__init__(name=name, inputs=inputs, outputs=outputs)
-        self.image = image
+            init_progress="0/1", timeout=None, retry_strategy=None, memoize_key=None):
         if command is None:
             command = ["python"]
-        self.command = command
-        self.script = script
-        if mounts is None:
-            mounts = []
-        self.mounts = mounts
-        self.init_progress = init_progress
-        self.timeout = timeout
-        self.retry_strategy = retry_strategy
-
-    def convert_to_argo(self):
-        return V1alpha1Template(name=self.name,
-            metadata=V1alpha1Metadata(annotations={"workflows.argoproj.io/progress": self.init_progress}),
-            inputs=self.inputs.convert_to_argo(),
-            outputs=self.outputs.convert_to_argo(),
-            timeout=self.timeout,
-            retry_strategy=self.retry_strategy,
-            script=V1alpha1ScriptTemplate(image=self.image, command=self.command, source=self.script, volume_mounts=self.mounts))
+        super().__init__(name=name, inputs=inputs, outputs=outputs, image=image, command=command, script=script,
+                mounts=mounts, init_progress=init_progress, timeout=timeout, retry_strategy=retry_strategy, memoize_key=memoize_key)
