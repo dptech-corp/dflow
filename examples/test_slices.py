0ea8adc6c9bc4ec9069936a1384763ded6fcce34
zjgemi <zjgemi@gmail.com>
2022-01-30 09:29:57 +0800

    add some examples

---
+++ examples/test_slices.py
@@ -0,0 +1,81 @@
+from dflow import (
+    Workflow,
+    Step,
+    argo_range
+)
+from dflow.python import (
+    PythonOPTemplate,
+    OP,
+    OPIO,
+    OPIOSign,
+    Artifact,
+    Slices
+)
+from typing import List
+
+class Hello(OP):
+    def __init__(self):
+        pass
+
+    @classmethod
+    def get_input_sign(cls):
+        return OPIOSign({
+            'filename': str
+        })
+
+    @classmethod
+    def get_output_sign(cls):
+        return OPIOSign({
+            'foo' : Artifact(str)
+        })
+
+    @OP.exec_sign_check
+    def execute(
+            self,
+            op_in : OPIO,
+    ) -> OPIO:
+        open(op_in["filename"], "w").write("foo")
+        op_out = OPIO({
+            'foo' : op_in["filename"]
+        })
+        return op_out
+
+class Check(OP):
+    def __init__(self):
+        pass
+
+    @classmethod
+    def get_input_sign(cls):
+        return OPIOSign({
+            'foo': Artifact(List[str])
+        })
+
+    @classmethod
+    def get_output_sign(cls):
+        return OPIOSign()
+
+    @OP.exec_sign_check
+    def execute(
+            self,
+            op_in : OPIO,
+    ) -> OPIO:
+        print(op_in["foo"])
+        return OPIO()
+
+if __name__ == "__main__":
+    wf = Workflow("slices")
+    hello = Step("hello",
+            PythonOPTemplate(Hello, image="dflow:v1.0",
+                    slices=Slices("{{item}}",
+                        input_parameter=["filename"],
+                        output_artifact=["foo"]
+                    )
+            ),
+            parameters={"filename": ["f1.txt", "f2.txt"]},
+            with_param=argo_range(2))
+    wf.add(hello)
+    check = Step("check",
+            PythonOPTemplate(Check, image="dflow:v1.0"),
+            artifacts={"foo": hello.outputs.artifacts["foo"]})
+    wf.add(check)
+    wf.submit()
---
+++ examples/test_success_ratio.py
@@ -0,0 +1,51 @@
+from dflow import (
+    Workflow,
+    Step,
+    upload_artifact,
+    download_artifact,
+    argo_range
+)
+from dflow.python import (
+    PythonOPTemplate,
+    OP,
+    OPIO,
+    OPIOSign,
+    Artifact,
+    TransientError,
+    FatalError
+)
+from typing import List
+from pathlib import Path
+import time
+import random
+
+class Hello(OP):
+    @classmethod
+    def get_input_sign(cls):
+        return OPIOSign()
+
+    @classmethod
+    def get_output_sign(cls):
+        return OPIOSign()
+
+    @OP.exec_sign_check
+    def execute(
+            self,
+            op_in : OPIO,
+    ) -> OPIO:
+        if random.random() < 0.5:
+            raise TransientError("Hello")
+        return OPIO()
+
+if __name__ == "__main__":
+    wf = Workflow(name="hello")
+
+    step = Step(
+        name="hello0", 
+        template=PythonOPTemplate(Hello, image="dflow:v1.0"), 
+        continue_on_success_ratio=0.6,
+        # continue_on_num_success=3,
+        with_param=argo_range(5)
+    )
+    wf.add(step)
+    wf.submit()
